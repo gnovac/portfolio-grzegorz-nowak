4daf90ebf1a0f0fba0db360d08d4a173dac1f26f
grzegorz_nowak <grzesiek6493@gmail.com>
2018-04-15 20:14:01 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -8,32 +8,9 @@ $(window).on('load', function () {
 });
 
 $(document).ready(function () {
-    activeLinks();
-    scrollArrow();
-
+    clickLinksActive();
+    typewriter();
 });
-
-$(window).scroll(function () {
-    activeScroll;
-});
-
-
-//========ACTIVE LINKS SWITCHING=======//
-function activeLinks() {
-    $("li").click(function () {
-        if (!$(this).hasClass("active")) {
-            $("li.active").removeClass("active");
-            $(this).addClass("active");
-        }
-    });
-}
-
-
-function scrollArrow() {
-    $('.cd-scroll-down').click(function () {
-        goDown();
-    });
-}
 
 //======PARALLAX EFFECT WITH SCROLL NAVIGATION=========//
 var ticking = false;
@@ -44,7 +21,7 @@ var slideDurationSetting = 600; //Amount of time for which slide is "locked"
 var currentSlideNumber = 0;
 var totalSlideNumber = $(".background").length;
 
-// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+// ------------- determine delta/scroll direction------------- //
 function wheelScroll(evt) {
     if (isFirefox) {
         //Set delta for Firefox
@@ -84,6 +61,8 @@ function goDown() {
     if (currentSlideNumber !== totalSlideNumber - 1) {
         currentSlideNumber++;
         nextItem();
+        activeSlide();
+        scrollLinksActive();
     }
     slideDurationTimeout(slideDurationSetting);
 }
@@ -95,11 +74,14 @@ function goUp() {
         currentSlideNumber--;
     }
     previousItem();
+    activeSlide();
+    scrollLinksActive();
     slideDurationTimeout(slideDurationSetting);
 
 }
 
-//navigations trought the slides
+
+//-----------navigations trought the slides------------//
 function goto(slide) {
     //slide numbering is like array from 0 to total-1
     if (currentSlideNumber == 0) {
@@ -176,14 +158,15 @@ function goto(slide) {
         }
     }
 }
-// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+
+// ------------- set timeout to temporatily "lock" slides------------- //
 function slideDurationTimeout(slideDuration) {
     setTimeout(function () {
         ticking = false;
     }, slideDuration);
 }
 
-// ------------- ADD EVENT LISTENER ------------- //
+// ------------- add event listener ------------- //
 var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
 var ts;
 
@@ -196,70 +179,79 @@ window.addEventListener("touchend", function (e) {
     touchScroll(ts, te);
 }, false);
 
-// ------------- SLIDE MOTION ------------- //
+// ------------- slide motion ------------- //
 function nextItem() {
-    var $previousSlide = $(".background").eq(currentSlideNumber - 1);
-    $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+    var previousSlide = $(".background").eq(currentSlideNumber - 1);
+    previousSlide.removeClass("up-scroll").addClass("down-scroll");
 }
 
 function previousItem() {
-    var $currentSlide = $(".background").eq(currentSlideNumber);
-    $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+    var currentSlide = $(".background").eq(currentSlideNumber);
+    currentSlide.removeClass("down-scroll").addClass("up-scroll");
 }
 
-
-function activeScroll() {
-    var scrollPos = $(window).scrollTop();
-    console.log(scrollPos);
+function activeSlide() {
+    var aSlide = $('.background').eq(currentSlideNumber);
+    aSlide.addClass('active');
+    aSlide.siblings().removeClass('active');
+    
 }
 
+//========ADD CLASS ACTIVE TO NAV - SCROLL & CLICK=======//
+function clickLinksActive() {
+    $("li").click(function () {
+        if (!$(this).hasClass("active")) {
+            $("li.active").removeClass("active");
+            $(this).addClass("active");
+        }
+    });
+}
 
-//-----------------------TYPEWRITER EFFECT-------------------------//
+function scrollLinksActive() {
+    var currentLink = $('li').eq(currentSlideNumber);
+    currentLink.addClass('active');
+    currentLink.siblings().removeClass('active');
+}
 
-
-
-
-// set up text to print, each item in array is new line
-var aText = new Array(
-    "&ltbody&gt",
-    "<p>&lth1&gt</p>",
-    "<h1>Hi,",
-    "I'm Gregory,",
+//====================TYPEWRITER EFFECT======================//
+var Text = new Array(
+    "<p>&ltbody&gt</p>",
+    "<h3>&lth1&gt</h3>",
+    "<h1>Hej,",
+    "Jestem Grzegorz,",
     "front end developer.</h1>",
-    "<p>&lt/h1&gt</p>",
-    "&lt/body&gt"
+    "<h3>&lt/h1&gt</h3>",
+    "<p>&lt/body&gt</p>"
 );
-var iSpeed = 100; // time delay of print out
-var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
-var iScrollAt = 20; // start scrolling up at this many lines
+var Speed = 100; // time delay of print out
+var Index = 0; // start printing array at this posision
+var ArrLength = Text[0].length; // the length of the text array
+var ScrollAt = 20; // start scrolling up at this many lines
 
-var iTextPos = 0; // initialise text position
-var sContents = ''; // initialise contents variable
-var iRow; // initialise current row
+var TextPos = 0; // initialise text position
+var Contents = ''; // initialise contents variable
+var Row; // initialise current row
 
 function typewriter() {
-    sContents = ' ';
-    iRow = Math.max(0, iIndex - iScrollAt);
-    var destination = document.getElementById("typedtext");
-    
+    Contents = ' ';
+    Row = Math.max(0, Index - ScrollAt);
+    var destination = $("#typedtext");
 
 
-    while (iRow < iIndex) {
-        sContents += aText[iRow++] + '<br />';
+
+    while (Row < Index) {
+        Contents += Text[Row++] + '<br />';
     }
-    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-    if (iTextPos++ == iArrLength) {
-        iTextPos = 0;
-        iIndex++;
-        if (iIndex != aText.length) {
-            iArrLength = aText[iIndex].length;
+    destination.html(Contents + Text[Index].substring(0, TextPos) + "<span>_</span>"); 
+    if (TextPos++ == ArrLength) {
+        TextPos = 0;
+        Index++;
+        if (Index != Text.length) {
+            ArrLength = Text[Index].length;
             setTimeout("typewriter()", 500);
         }
     } else {
-        setTimeout("typewriter()", iSpeed);
+        setTimeout("typewriter()", Speed);
     }
 }
 
-
-typewriter();
