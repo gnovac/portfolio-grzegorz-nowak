b25d70cd27df3cbd94ffdc21a6dbe6c89fc401ce
grzegorz_nowak <grzesiek6493@gmail.com>
2018-04-21 22:42:04 +0200

    contact section added

--- js/main.js
+++ js/main.js
@@ -255,3 +255,17 @@ function typewriter() {
         setTimeout("typewriter()", Speed);
     }
 }
+
+
+//================GOOGLE MAP API=================//
+function initMap() {
+        var uluru = {lat: 50.06465009999999, lng: 19.94497990000002};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
